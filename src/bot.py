62180d586b925b95d3a613849feeac060698439b
Riley Kuttruff <72955101+RKuttruff@users.noreply.github.com>
2022-02-17 21:31:14 -0800

    Update bot.py
    Basic implementation

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,40 @@
 import os
+import random
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+bot = commands.Bot(command_prefix='!')
 
+@bot.event
+async def on_ready():
+	print(f'{bot.user.name} has connected to Discord!')
+
+# @bot.command(name='roll_dice')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+	dice = [
+		str(random.choice(range(1, number_of_sides + 1)))
+		for _ in range(number_of_dice)
+	]
+	await ctx.send(', '.join(dice))
+
+@bot.command(name='roll_dice', help='Rolls dice with the numbers of faces given several die can be rolled in one command')
+async def roll_die(ctx, *args):
+	total = 0
+	valid = False
+	s = ''
+	for arg in args:
+		if int(arg) > 0:
+			valid = True
+			r = random.randint(1, int(arg))
+			s = s + str(r) + ' '
+			total += r
+
+	if valid:
+#		print(s.split())
+		await ctx.send('You rolled: ' + ', '.join(s.split()) + '; total = ' + str(total))
+
+bot.run(TOKEN)
