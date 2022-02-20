d5d5d4ddede493cc0d2713b69dd4dce57c7de10f
Riley Kuttruff <72955101+RKuttruff@users.noreply.github.com>
2022-02-20 02:18:55 -0800

    Update bot.py
    Added methods for commands and io

--- src/bot.py
+++ src/bot.py
@@ -9,12 +9,24 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
+aliases = {}
+
+# Function to read in aliases from statefile.
+# Currently a placeholder
+def init():
+	print('init')
+
+# Function to write out aliases to statefile.
+# Currently a placeholder
+def stateWrite():
+	print('stateWrite')
+
+
 @bot.event
 async def on_ready():
-	print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+	print(f'{bot.user} is connected to the following guild:\n')
+	for guild in bot.guilds:
+		print(f'{guild.name}(id: {guild.id})')
 
 @bot.command(name='roll_dice', help='Rolls dice with the numbers of faces given several die can be rolled in one command')
 async def roll_die(ctx, *args):
@@ -31,5 +43,17 @@ async def roll_die(ctx, *args):
 	if valid:
 #		print(s.split())
 		await ctx.send('You rolled: ' + ', '.join(s.split()) + '; total = ' + str(total))
+
+
+@bot.command(name='alias', help='Assign, remove and manage aliases')
+async def alias(ctx, *args):
+	print('alias command called')
+
+@bot.command(name='roll', help='Roll a set of dice defined by a given alias')
+async def roll(ctx, alias):
+	print('roll command called')
+
+
+init()
 
 bot.run(TOKEN)
