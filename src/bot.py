09ed1f727e7707cc54f0e0cc598f35840a6d44f1
Riley Kuttruff <72955101+RKuttruff@users.noreply.github.com>
2022-02-27 21:52:20 -0800

    Update bot.py
    Import socket

--- src/bot.py
+++ src/bot.py
@@ -8,7 +8,7 @@
 #
 #	You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import os, sys, random, json, zipfile
+import os, sys, random, json, zipfile, socket
 
 from threading import Thread
 
